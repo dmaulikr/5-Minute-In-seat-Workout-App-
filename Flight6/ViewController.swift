4edea6e8f62366a3a83b44aabc30130ebe51fc5d
Hashma <shahi22h@mtholyoke.edu>
2016-06-19 10:22:44 +0500

    Initial Commit

---
+++ Flight6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flight6
+//
+//  Created by Hashma Shahid on 6/19/16.
+//  Copyright (c) 2016 Hashma Shahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
