c8949eb0310c591580640149bb295a11f6988cea
Hashma <shahi22h@mtholyoke.edu>
2016-06-19 16:09:46 +0500

    “update”

--- Flight6/WorkoutViewController.swift
+++ Flight6/WorkoutViewController.swift
@@ -15,11 +15,11 @@ class WorkoutViewController: UIViewController, MZTimerLabelDelegate {
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var timerLabel: MZTimerLabel!
     
-    @IBOutlet weak var currentWorkout: UILabel!
+    @IBOutlet weak var workoutLabel: UILabel!
     
+    @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var nextUp: UILabel!
     
-    @IBOutlet weak var nextWorkout: UILabel!
     
     
     @IBAction func pauseResume(sender: UIButton) {
@@ -56,7 +56,7 @@ class WorkoutViewController: UIViewController, MZTimerLabelDelegate {
                     timerLabel.resetTimerAfterFinish = true
                     timerLabel.start()
             workouts = dataSource.getWorkOuts()
-            currentWorkout.text = workouts[0].title
+            workoutLabel.text = workouts[0].title
           self.navigationController?.navigationBar.barTintColor = workouts[0].color
             self.navigationController?.navigationBar.backgroundColor = workouts[0].color
             self.navigationController?.navigationBar.titleTextAttributes = ([NSFontAttributeName: UIFont(name: "HelveticaNeue-Light", size: 36)!, NSForegroundColorAttributeName: UIColor.whiteColor()])
@@ -67,11 +67,7 @@ class WorkoutViewController: UIViewController, MZTimerLabelDelegate {
             
             
         }
-        
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-        }
-        
+    
         func buttonUI(button: UIButton) {
             button.backgroundColor = UIColor.clearColor()
             button.layer.borderWidth = 1.0
@@ -79,55 +75,37 @@ class WorkoutViewController: UIViewController, MZTimerLabelDelegate {
             button.layer.cornerRadius = cornerRadius
             
         }
-        
-        func timerLabel(timerLabel: MZTimerLabel!, finshedCountDownTimerWithTime countTime: NSTimeInterval) {
-            
-            if index % 2 == 0 {
-                let wk = workouts[index / 2]
-                timerLabel.text = "\(workoutTime)"
-                currentWorkout.text = wk.title
-                self.view.backgroundColor = wk.color
-                timerLabel.setCountDownTime(workoutTime);
-                nextWorkout.text = "Rest !!"
-               // self.navigationController?.navigationBar.barTintColor = wk.color
-                
-                UINavigationBar.appearance().setBackgroundImage(UIImage(), forBarMetrics: .Default)
-                // Sets shadow (line below the bar) to a blank image
-                UINavigationBar.appearance().shadowImage = UIImage()
-                // Sets the translucent background color
-                UINavigationBar.appearance().backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
-                // Set translucent. (Default value is already true, so this can be removed if desired.)
-                UINavigationBar.appearance().translucent = true
-                
-
-                
-                
-            } else {
-                let wk = workouts[index / 2 + 1]
-                timerLabel.text = "\(restTime)"
-                currentWorkout.text = "Rest !!"
-                self.view.backgroundColor = wk.color
-                timerLabel.setCountDownTime(restTime);
-                nextWorkout.text = wk.title
-               // self.navigationController?.navigationBar.barTintColor = wk.color
-                UINavigationBar.appearance().setBackgroundImage(UIImage(), forBarMetrics: .Default)
-                // Sets shadow (line below the bar) to a blank image
-                UINavigationBar.appearance().shadowImage = UIImage()
-                // Sets the translucent background color
-                UINavigationBar.appearance().backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
-                // Set translucent. (Default value is already true, so this can be removed if desired.)
-                UINavigationBar.appearance().translucent = true
-                
-
-            }
-            
-            index++;
-            if !((index/2) > workouts.count) {
-                timerLabel.start()
-            } else {
-                println("Workout done")
-            }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
+    func timerLabel(timerLabel: MZTimerLabel!, finshedCountDownTimerWithTime countTime: NSTimeInterval) {
+        
+        if index % 2 == 0 {
+            let wk = workouts[index / 2]
+            timerLabel.text = "\(workoutTime)"
+            mainLabel.text = wk.title
+            self.view.backgroundColor = wk.color
+            timerLabel.setCountDownTime(workoutTime);
+            workoutLabel.text = "Rest !!"
+            self.navigationController?.navigationBar.barTintColor = wk.color
+        } else {
+            let wk = workouts[index / 2 + 1]
+            timerLabel.text = "\(restTime)"
+            mainLabel.text = "Rest !!"
+            self.view.backgroundColor = wk.color
+            timerLabel.setCountDownTime(restTime);
+            workoutLabel.text = wk.title
+            self.navigationController?.navigationBar.barTintColor = wk.color
+        }
+        
+        index++;
+        if !((index / 2) > workouts.count) {
+            timerLabel.start()
+        } else {
+            println("Workout done")
+        }
+    }
     
 }
