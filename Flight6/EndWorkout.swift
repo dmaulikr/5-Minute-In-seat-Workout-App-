8548f4654193bc561108c219dbdd77f5340aa3fb
Hashma <shahi22h@mtholyoke.edu>
2016-06-23 00:29:55 +0500

    “update”

--- Flight6/EndWorkout.swift
+++ Flight6/EndWorkout.swift
@@ -9,5 +9,30 @@
 import UIKit
 
 class EndWorkout: UIViewController {
+    
+    @IBOutlet weak var endWorkoutLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let color7 = UIColor.flatNavyBlueColorDark()
+        let color1 = UIColor(hue: 0.9528, saturation: 0.4, brightness: 1, alpha: 1.0) /* #ff759d */
+        setTableViewBackgroundGradient(color7, bottomColor: color1);
+    }
+    
+    func setTableViewBackgroundGradient(topColor: UIColor, bottomColor: UIColor){
+        
+        let gradientBackgroundColors = [topColor.CGColor, bottomColor.CGColor]
+        let gradientLocations = [0.0,1.0]
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientBackgroundColors
+        gradientLayer.locations = gradientLocations
+        
+        gradientLayer.frame = view.bounds
+        let backgroundView = UIView(frame: view.bounds)
+        backgroundView.layer.insertSublayer(gradientLayer, atIndex: 0)
+        view.addSubview(backgroundView)
+        
+    }
+
 
 }
