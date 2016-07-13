21e4877893838feec0aa70fc7d90cd5e7f240aa0
Hashma <shahi22h@mtholyoke.edu>
2016-07-13 22:08:14 +0500

    cleaning up code according to convention

--- Flight6/WorkoutViewController.swift
+++ Flight6/WorkoutViewController.swift
@@ -68,7 +68,7 @@ class WorkoutViewController: UIViewController, MZTimerLabelDelegate {
             self.navigationController?.navigationBar.titleTextAttributes = ([NSFontAttributeName: UIFont(name: "HelveticaNeue-Light", size: 36)!, NSForegroundColorAttributeName: UIColor.whiteColor()])
              view.backgroundColor = UIColor.flatNavyBlueColorDark()
             setButtonStyle(pauseButton)
-            setButtonStyle(<#button: UIButton#>)
+            setButtonStyle(backHome)
         }
     
         func setButtonStyle(button: UIButton) {
